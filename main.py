dc563430135d5ba5f826facc5fc2dfd959b02b01
Miguel Lopez <ing.miguell@gmail.com>
2023-03-29 16:46:14 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,33 @@
+menu = 0
+
+if menu == 1:
+    # mostrar categorias
+    # elegir categoria
+    # mostrar recetas
+    # elegir receta
+    # mostrar receta
+    # volver a inicio
+    pass
+elif menu == 2:
+    # mostrar categorias
+    # elegir categoria
+    # crear receta
+    # volver a inicio
+    pass
+elif menu == 3:
+    # crear categoria
+    # volver a inicio
+    pass
+elif menu == 4:
+    # mostrar categorias
+    # elegir categoria
+    # mostrar recetas
+    # elegir receta
+    # eliminar receta
+    # volver a inicio
+    pass
+elif menu == 5:
+    # mostrar categorias
+    # elegir categoria
+    # volver a inicio
+    pass
